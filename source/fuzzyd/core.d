df0ed3b7dc47eb53041432b329a93538d829fc3c
Felipe Beline Baravieira <felipe@fbeline.com>
2020-04-15 21:06:18 -0300

    Change matches from array to RedBlackTree

--- source/fuzzyd/core.d
+++ source/fuzzyd/core.d
@@ -7,58 +7,66 @@ import std.container.binaryheap;
 import std.ascii;
 import std.algorithm.iteration;
 
-alias Result[] delegate(string) scoreFn;
+alias Result[]delegate(string) scoreFn;
 alias int function(Input) bonusFn;
 
-export struct Result {
-  string value;
-  int score;
-  int[] matches;
+struct Result
+{
+    string value;
+    int score;
+    RedBlackTree!(int, "a < b", false) matches;
 }
 
-struct Input {
-  string input;
-  string pattern;
-  int col;
-  int row;
-  int[][] scoreMatrix;
+struct Input
+{
+    string input;
+    string pattern;
+    int col;
+    int row;
+    int[][] scoreMatrix;
 
-  char inputAtIndex() {
-    return input[row];
-  }
+    char inputAtIndex()
+    {
+        return input[row];
+    }
 
-  char patternAtIndex() {
-    return pattern[col];
-  }
+    char patternAtIndex()
+    {
+        return pattern[col];
+    }
 
-  char isMatch() {
-    return toLower(inputAtIndex) == toLower(patternAtIndex);
-  }
+    char isMatch()
+    {
+        return toLower( inputAtIndex) == toLower( patternAtIndex);
+    }
 
-  char isCaseSensitiveMatch() {
-    return isUpper(inputAtIndex) && isUpper(patternAtIndex) && isMatch ? 1 : 0;
-  }
+    char isCaseSensitiveMatch()
+    {
+        return isUpper( inputAtIndex) && isUpper( patternAtIndex) && isMatch ? 1 : 0;
+    }
 }
 
-int previousCharBonus(Input input) {
-  return (input.col > 0 && input.row > 0) ?
-    2 * input.scoreMatrix[input.row-1][input.col-1] : 0;
+int previousCharBonus(Input input)
+{
+    return (input.col > 0 && input.row > 0) ? 2 * input.scoreMatrix[input.row - 1][input.col - 1]
+    : 0;
 }
 
-int startBonus(Input input) {
-  return (input.col == 0 && input.row == 0) ? 1 : 0;
+int startBonus(Input input)
+{
+    return (input.col == 0 && input.row == 0) ? 1 : 0;
 }
 
-int caseMatchBonus(Input input) {
-  return input.isCaseSensitiveMatch ? 1 : 0;
+int caseMatchBonus(Input input)
+{
+    return input.isCaseSensitiveMatch ? 1 : 0;
 }
 
-int wordBoundaryBonus(Input input) {
-  bool isInputAt = input.row == 0 ||
-    input.row == input.input.length-1 ||
-    isWhite(input.input[input.row-1]) ||
-    isWhite(input.input[input.row+1]);
-  return isInputAt ? 1 : 0;
+int wordBoundaryBonus(Input input)
+{
+    bool isInputAt = input.row == 0 || input.row == input.input.length - 1
+    || isWhite( input.input[input.row - 1]) || isWhite( input.input[input.row + 1]);
+    return isInputAt ? 1 : 0;
 }
 
 /**
@@ -72,39 +80,48 @@ int wordBoundaryBonus(Input input) {
  * // => [Result("bar", 5, [0, 2]), Result("baz", 3, [0]), Result("foo", 0, [])]
  * --------------------
  */
-public scoreFn fuzzy(string[] db) {
+public scoreFn fuzzy(string[] db)
+{
 
-  bonusFn[] bonusFns = [&previousCharBonus, &startBonus, &caseMatchBonus, &wordBoundaryBonus];
+    immutable bonusFn[] bonusFns = [
+    &previousCharBonus, &startBonus, &caseMatchBonus, &wordBoundaryBonus
+    ];
 
-  int charScore(Input input) {
-    return input.isMatch ?
-      reduce!((acc, f) => acc + f(input))(1, bonusFns) : 0;
-  }
-
-  Result score(string input, string pattern) {
-    int score = 0;
-    auto matches = redBlackTree!int();
-    int[][] scoreMatrix = new int[][](input.length, pattern.length);
-
-    for (int col = 0; col < pattern.length; col++) {
-      for (int row = 0; row < input.length; row++) {
-        int charScore = charScore(Input(input, pattern, col, row, scoreMatrix));
-        if (charScore > 0) matches.insert(row);
-        score += charScore;
-        scoreMatrix[row][col] = charScore;
-      }
+    int charScore(Input input)
+    {
+        return input.isMatch ? reduce!((acc, f) => acc + f( input))( 1, bonusFns) : 0;
     }
 
-    return Result(input, score, matches.array());
-  }
+    Result score(string input, string pattern)
+    {
+        int score = 0;
+        auto matches = redBlackTree!int();
+        int[][] scoreMatrix = new int[][]( input.length, pattern.length);
 
-  Result[] search(string pattern) {
-    auto maxpq = BinaryHeap!(Result[], "a.score < b.score")(new Result[db.length], 0);
-    foreach(e; db) {
-      maxpq.insert(score(e, pattern));
+        for (int col = 0; col < pattern.length; col++)
+        {
+            for (int row = 0; row < input.length; row++)
+            {
+                int charScore = charScore( Input( input, pattern, col, row, scoreMatrix));
+                if (charScore > 0)
+                    matches.insert( row);
+                score += charScore;
+                scoreMatrix[row][col] = charScore;
+            }
+        }
+
+        return Result( input, score, matches);
     }
-    return maxpq.array();
-  }
 
-  return &search;
+    Result[] search(string pattern)
+    {
+        auto maxpq = BinaryHeap!(Result[], "a.score < b.score")( new Result[db.length], 0);
+        foreach (e; db)
+        {
+            maxpq.insert( score( e, pattern));
+        }
+        return maxpq.array();
+    }
+
+    return &search;
 }
--- tests/fuzzy_test.d
+++ tests/fuzzy_test.d
@@ -1,54 +1,59 @@
 module fuzzyd_test;
 
 import std.stdio;
+import std.array;
 import std.algorithm;
 import std.algorithm.comparison : equal;
 
 import fuzzyd.core;
 
-Result[] prepare(string s) {
-  string[] source = ["cd Documents",
-                     "curl localhost/foo",
-                     "cp bar ../foo",
-                     "rm -rf Downloads",
-                     "vi ~/Documents"];
-  return fuzzy(source)(s);
+Result[] prepare(string s)
+{
+    string[] source = [
+    "cd Documents", "curl localhost/foo", "cp bar ../foo",
+    "rm -rf Downloads", "vi ~/Documents"
+    ];
+    return fuzzy( source)( s);
 }
 
 @("Matches in expected order")
-unittest {
-  auto result = prepare("docts").map!(x => x.value);
-  auto expected = ["cd Documents",
-                   "vi ~/Documents",
-                   "curl localhost/foo",
-                   "rm -rf Downloads",
-                   "cp bar ../foo"];
-  assert(equal(expected, result));
+unittest
+{
+    auto result = prepare( "docts").map!(x => x.value);
+    auto expected = [
+    "cd Documents", "vi ~/Documents", "curl localhost/foo",
+    "rm -rf Downloads", "cp bar ../foo"
+    ];
+    assert(equal( expected, result));
 }
 
 @("Matches indexes")
-unittest {
-  auto result = prepare("docts")[0].matches;
-  auto expected = [0, 1, 3, 4, 5, 10, 11];
-  assert(equal(expected, result));
+unittest
+{
+    auto result = prepare( "docts")[0].matches.array();
+    auto expected = [0, 1, 3, 4, 5, 10, 11];
+    assert(equal( expected, result));
 }
 
 @("Start bonus is applied")
-unittest {
-  auto result = prepare("curl")[0].score;
-  assert(61 == result);
+unittest
+{
+    auto result = prepare( "curl")[0].score;
+    assert(61 == result);
 }
 
 @("Case bonus is applied")
-unittest {
-  auto r1 = prepare("docts")[0].score;
-  auto r2 = prepare("Docts")[0].score;
-  assert(r1 == 27);
-  assert(r2 == 34);
+unittest
+{
+    auto r1 = prepare( "docts")[0].score;
+    auto r2 = prepare( "Docts")[0].score;
+    assert(r1 == 27);
+    assert(r2 == 34);
 }
 
 @("Word boundary bonus is applied")
-unittest {
-  auto result = prepare("cd")[0].score;
-  assert(14 == result);
+unittest
+{
+    auto result = prepare( "cd")[0].score;
+    assert(14 == result);
 }
